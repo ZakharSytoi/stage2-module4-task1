adf42ca83fea507d32d58c014b3896d23c2b0fbb
ZakharSytoi <zakharsytoi@gmail.com>
2023-06-28 18:14:44 +0200

    Factory task completed

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,37 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
     // Write your code here!
+
+
+    @Override
+    public Connection createConnection() {
+        Properties props = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = loader.getResourceAsStream("h2database.properties");
+        try {
+            props.load(stream);
+            try {
+                Class.forName(props.getProperty("jdbc_driver"));
+                return DriverManager.getConnection(
+                        props.getProperty("db_url")
+                        , props.getProperty("user")
+                        , props.getProperty("password"));
+            } catch (ClassNotFoundException | SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
