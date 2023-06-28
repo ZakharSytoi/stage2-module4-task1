40e3bbc71e41bd5ba06cf7eb3405d07386e6ab0d
ZakharSytoi <zakharsytoi@gmail.com>
2023-06-28 18:22:14 +0200

    singleton task thread safety fixed

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,13 +2,16 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
-    private static final ThreadSafeSingleton instance = new ThreadSafeSingleton();;
+    private static final ThreadSafeSingleton instance
 
     private ThreadSafeSingleton(){
 
     }
 
-    public static ThreadSafeSingleton getInstance(){
+    public synchronized static ThreadSafeSingleton getInstance(){
+        if (instance == null) {
+            return new ThreadSafeSingleton();
+        }
         return instance;
     }
 }
